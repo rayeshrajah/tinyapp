f1d73da4d88d5162f36d670084168d2ea70998cf
Rayesh <rayeshrajah@gmail.com>
2020-02-17 22:08:29 +0000

    short url using req.params is done

--- express_server.js
+++ express_server.js
@@ -8,9 +8,13 @@ const urlDatabase = {
     'b2xVn2': 'www.lighthouselabs.ca',
     '9sm5xK': 'www.google.ca'
 }
-app.get('/urls', (req, res) => {
-    let templateUrl = {urls: urlDatabase}
-    res.render('urls_index', templateUrl);
+// app.get('/urls', (req, res) => {
+//     let templateUrl = {urls: urlDatabase}
+//     res.render('urls_index', templateUrl);
+// });
+app.get(`/urls/:shortURL`, (req, res) => {
+    let templateVar = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+    res.render('urls_show.ejs', templateVar);
 });
 
 app.listen(PORT, () => {
