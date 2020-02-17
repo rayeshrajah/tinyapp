fe4efd55faf8e1ae07cf56526ec716c5b6e5445c
Rayesh <rayeshrajah@gmail.com>
2020-02-17 19:49:44 +0000

    testing some stuff

--- express_server.js
+++ express_server.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.send('Hello!');
 });
 
+app.get('/urls.json', (req, res) => {
+    res.send(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+    res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
 });
