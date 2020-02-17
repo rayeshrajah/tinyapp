fe5f9e83ac802dcf2459d78de7ee5ff58dd96b01
Rayesh <rayeshrajah@gmail.com>
2020-02-17 20:38:50 +0000

    added ejs dependecies

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
     'b2xVn2': 'www.lighthouselabs.ca',
     '9sm5xK': 'www.google.ca'
@@ -16,6 +18,15 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
     res.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/set', (req, res) => {
+    const a = 1
+    res.send(`Assigning a variable \'a\'  which contains the number 1 in this path: ${a}`);
+});
+
+app.get('/fetch', (req, res) => {
+    res.send(`fetching some stuff from \'\set\' path: ${a}`);
 });
 
 app.listen(PORT, () => {
