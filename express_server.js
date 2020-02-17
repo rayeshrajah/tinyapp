0a8c1d6e88820f5ebf0623b774fabb303b7fea9e
Rayesh <rayeshrajah@gmail.com>
2020-02-17 21:07:47 +0000

    Added ejs files and made short and long url table in urls_index.js file

--- express_server.js
+++ express_server.js
@@ -8,27 +8,32 @@ const urlDatabase = {
     'b2xVn2': 'www.lighthouselabs.ca',
     '9sm5xK': 'www.google.ca'
 }
-app.get('/', (req, res) => {
-    res.send('Hello!');
-});
-
-app.get('/urls.json', (req, res) => {
-    res.send(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-    res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
-app.get('/set', (req, res) => {
-    const a = 1
-    res.send(`Assigning a variable \'a\'  which contains the number 1 in this path: ${a}`);
-});
-
-app.get('/fetch', (req, res) => {
-    res.send(`fetching some stuff from \'\set\' path: ${a}`);
+app.get('/urls', (req, res) => {
+    let templateUrl = {urls: urlDatabase}
+    res.render('urls_index', templateUrl);
 });
 
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`);
 });
+
+// app.get('/', (req, res) => {
+//     res.send('Hello!');
+// });
+
+// app.get('/urls.json', (req, res) => {
+//     res.send(urlDatabase);
+// });
+
+// app.get('/hello', (req, res) => {
+//     res.send('<html><body>Hello <b>World</b></body></html>\n');
+// });
+
+// app.get('/set', (req, res) => {
+//     const a = 1
+//     res.send(`Assigning a variable \'a\'  which contains the number 1 in this path: ${a}`);
+// });
+
+// app.get('/fetch', (req, res) => {
+//     res.send(`fetching some stuff from \'\set\' path: ${a}`);
+// });
